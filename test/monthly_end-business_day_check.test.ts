e5cc7297df1658d4247e9515b1cf0987c1d746b0
ko-uema2 <uematsuku@gmail.com>
2025-06-16 22:35:05 +0900

    Initial commit

---
+++ test/monthly_end-business_day_check.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MonthlyEndBusinessDayCheck from '../lib/monthly_end-business_day_check-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/monthly_end-business_day_check-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MonthlyEndBusinessDayCheck.MonthlyEndBusinessDayCheckStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
