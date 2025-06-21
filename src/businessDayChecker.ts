6f3ca6bae20db03cca71017b95023e1d06d7f12a
ko-uema2 <uematsuku@gmail.com>
2025-06-22 00:59:18 +0900

    add ビジネスロジックを実装

---
+++ src/businessDayChecker.ts
@@ -0,0 +1,36 @@
+import { format, subDays } from "date-fns";
+import { isWeekend } from "date-fns";
+import type { calendar_v3 } from "googleapis";
+
+export class BusinessDayChecker {
+	constructor(private readonly holidays: calendar_v3.Schema$Event[]) {}
+
+	isLastBusinessDay(date: Date): boolean {
+		const lastBusinessDay = this.getLastBusinessDayOfMonth(date);
+		return format(date, "yyyy-MM-dd") === format(lastBusinessDay, "yyyy-MM-dd");
+	}
+
+	private getLastBusinessDayOfMonth(date: Date): Date {
+		let checkingDate = new Date(date.getFullYear(), date.getMonth() + 1, 0); // 当月の末日
+
+		while (this.isHoliday(checkingDate)) {
+			checkingDate = subDays(checkingDate, 1);
+		}
+
+		return checkingDate;
+	}
+
+	private isHoliday(date: Date): boolean {
+		// 土日の判定
+		if (isWeekend(date)) {
+			return true;
+		}
+
+		// 祝日の判定
+		return this.holidays.some(
+			(holiday) =>
+				new Date(holiday.start?.dateTime ?? "").toDateString() ===
+				date.toDateString(),
+		);
+	}
+}
